8b2b45fdb3938d5884377e8024fb30433639b8d7
= <=>
2021-04-12 13:46:48 +0300

    Counter Updated

---
+++ 2-Counter/app.js
@@ -0,0 +1,31 @@
+// Set Initial Count
+let count = 0;
+
+//Select Value And Buttons
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll(".btn");
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function(e){
+        const styles = e.currentTarget.classList;
+        if (styles.contains("decrease")){
+            count--;
+        }
+        else if(styles.contains("increase")){
+            count++
+        }
+        else{
+            count = 0
+        }
+        if (count > 0) {
+            value.style.color = "green";
+        }
+        if(count < 0) {
+            value.style.color = "red";
+        }
+        if (count === 0) {
+            value.style.color = "#222";
+        }
+        value.textContent = count;
+    })
+});
+
