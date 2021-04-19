938a1abefa2bf031343727e222d8120188f25d94
= <=>
2021-04-19 11:50:49 +0300

    updated

--- 8-Menu/app.js
+++ 8-Menu/app.js
@@ -71,18 +71,30 @@ const menu = [
     img: "./images/item-9.jpeg",
     desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
   },
+  {
+    id: 10,
+    title: "steak dinner",
+    category: "dinner",
+    price: 39.99,
+    img: "./images/item-10.jpeg",
+    desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+  },
 ];
 
 const sectionCenter = document.querySelector(".section-center");
 
-window.addEventListener("DOMContentLoaded", function(){
- displayMenuItems(menu)
+const container = document.querySelector(".btn-container");
+
+//load Items
+window.addEventListener("DOMContentLoaded", function () {
+  displayMenuItems(menu);
+  displayMenuButtons();
 });
 
-function displayMenuItems(menuItems){
-   let displayMenu = menuItems.map(function (item) {
-     console.log(item);
-     return `  <article class="menu-item">
+function displayMenuItems(menuItems) {
+  let displayMenu = menuItems.map(function (item) {
+    //  console.log(item);
+    return `  <article class="menu-item">
           <img src=${item.img} class="photo" alt=${item.title} />
           <div class="item-info">
             <header>
@@ -94,8 +106,45 @@ function displayMenuItems(menuItems){
             </p>
           </div>
         </article>`;
-   });
-   displayMenu = displayMenu.join("");
+  });
+  displayMenu = displayMenu.join("");
 
-   sectionCenter.innerHTML = displayMenu;
+  sectionCenter.innerHTML = displayMenu;
+}
+
+function displayMenuButtons() {
+  const categories = menu.reduce(
+    function (values, item) {
+      if (!values.includes(item.category)) {
+        values.push(item.category);
+      }
+      return values;
+    },
+    ["all"]
+  );
+  const categoryBtns = categories
+    .map(function (category) {
+      return `<button class="filter-btn" type="button" data-id=${category}>${category}</button>`;
+    })
+    .join("");
+  container.innerHTML = categoryBtns;
+  const filterBtns = container.querySelectorAll(".filter-btn");
+  //Filter items
+  filterBtns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+      const category = e.currentTarget.dataset.id;
+      const menuCategory = menu.filter(function (menuItem) {
+        // console.log(menuItem.category)
+        if (menuItem.category === category) {
+          return menuItem;
+        }
+      });
+      // console.log(menuCategory);
+      if (category === "all") {
+        displayMenuItems(menu);
+      } else {
+        displayMenuItems(menuCategory);
+      }
+    });
+  });
 }
