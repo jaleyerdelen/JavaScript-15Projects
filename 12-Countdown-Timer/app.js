4313934dfa379f67c61760b2d13dbce5dd41d781
jaleyerdelen <jaleyerdelen@gmail.com>
2021-05-18 22:09:44 +0300

    Date Updated

--- 12-Countdown-Timer/app.js
+++ 12-Countdown-Timer/app.js
@@ -26,7 +26,7 @@ const giveaway = document.querySelector(".giveaway");
 const deadline = document.querySelector(".deadline");
 const items = document.querySelectorAll(".deadline-format h4");
 
-let futureDate = new Date(2020, 3, 25, 11, 30, 0);
+let futureDate = new Date(2021, 05, 18, 11, 30, 0);
 console.log(futureDate )
 const year = futureDate.getFullYear();
 const hours = futureDate.getHours();
@@ -36,7 +36,28 @@ let month = futureDate.getMonth();
 month = months[month];
 const date = futureDate.getDate();
 
-const weekday = weekdays [futureDate.getDay()];
+const weekday = weekdays[futureDate.getDay()];
 console.log(weekday)
 
 giveaway.textContent = `giveaway ends on ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+
+// future time in ms 
+const futureTime = futureDate.getTime();
+
+
+function getRemainingTime() {
+  const today = new Date().getTime();
+  const t = futureTime - today;
+  console.log(t);
+
+  // values in ms
+  const oneDay = 24 * 60 * 60 * 1000;
+  const oneHour = 60 * 60 * 1000;
+  const oneMinute = 60 * 1000;
+
+  //calculate all values
+  let days = t / oneDay;
+  // console.log(days);
+}
+
+getRemainingTime();
