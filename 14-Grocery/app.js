6283efd1bf5b37cc6d05e1f35893fc97cd0a8763
jaleyerdelen <jaleyerdelen@gmail.com>
2021-06-28 12:08:15 +0300

    Updated grocery

--- 14-Grocery/app.js
+++ 14-Grocery/app.js
@@ -17,14 +17,44 @@ let editID = "";
 form.addEventListener("submit", addItem);
 //***Functions***
 function addItem(e) {
-    e.preventDefault();
-    const value = grocery.value
-    const id = new Date().getTime().toString();
-    if(value !== "" && editFlag === false){
-        console.log("add item to the list");
-    } else if(value !== "" && editFlag === true) {
-        console.log("editing");
-    }else {
-        console.log("empty value");
-    }
+  e.preventDefault();
+  const value = grocery.value;
+  const id = new Date().getTime().toString();
+  if (value && !editFlag) {
+    const element = document.createElement("article");
+    //add class
+    element.classList.add("grocery-item");
+    //add id
+    const attr = document.createAttribute("data-id");
+    attr.value = id;
+    element.setAttributeNode(attr);
+    element.innerHTML = ` <p class="title">${value}</p>
+            <div class="btn-container">
+              <button type="button" class="edit-btn">
+                <i class="fas fa-edit"></i>
+              </button>
+              <button type="button" class="delete-btn">
+                <i class="fas fa-trash"></i>
+              </button>
+            </div>`;
+    //append child
+    list.appendChild(element);
+    //display alert
+    displayAlert("item added to the list", "success");
+  } else if (value && editFlag) {
+    console.log("editing");
+  } else {
+    displayAlert("Please enter value", "danger");
+  }
+}
+//display alert
+function displayAlert(text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+
+  //remove alert
+  setTimeout(function () {
+    alert.textContent = "";
+    alert.classList.remove(`alert-${action}`);
+  }, 1000);
 }
